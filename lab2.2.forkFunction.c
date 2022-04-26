efc5b9cbf82565df207bc57ffbf1c13be0c68c00
bbxiuu <2021833992@student.uitm.edu.my>
2022-04-26 14:17:54 +0800

    Lab 2.2 for return pid

---
+++ lab2.2.forkFunction.c
@@ -0,0 +1,31 @@
+#include <stdlib.h> /* needed to define exit()*/
+#include <unistd.h> /* needed for fork() and getpid() */
+#include <stdio.h> /* needed for printf() */
+
+int main(int argc, char **argv){
+
+int pid; /*process id */
+
+
+switch(pid=fork()){
+
+case 0: /* a fork returns 0 to child */
+
+	printf("I am the child process : pid=%d\n", getpid());
+	break;
+
+default: /* a fork returns pid to the parent */
+
+	printf("I am the parent process: pid=%d, child pid=%d\n", getpid(),pid);
+	break;
+
+case-1: /*something went wrong */
+
+	perror("fork");
+	exit(1);
+
+}
+
+exit(0);
+
+}
