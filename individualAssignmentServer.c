cc2f022cf218400f71a5f3037134d1e66a41838b
bbxiuu <bbxiuu@github.com>
2022-05-24 20:41:22 +0800

    server side

--- individualAssignmentServer.c
+++ individualAssignmentServer.c
@@ -18,4 +18,32 @@ exit(0);
 int port = atoi(argv[1]);
 printf("Port: %d\n", port);
 
+int n_client = 0;
+int sockfd = socket(AF_INET,SOCK_STREAM,0);
+struct sockaddr_in serverAddress;
+serverAddress.sin_family = AF_INET;
+serverAddress.sin_addr.s_addr = INADDR_ANY;
+serverAddress.sin_port = htons(port);
+
+
+bind(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+printf("[+]Bind\n");
+
+listen(sockfd, BACKLOG);
+printf("[+]Listeing for the client\n");
+
+int i = 1;
+while(i){
+int client_socket = accept(sockfd, NULL, NULL);
+n_client++;
+time_t currentTime;
+time(&currentTime);
+printf("Client %d requested for time at %s", n_client, ctime(&currentTime));
+send(client_socket, ctime(&currentTime), 30, 0);
+}
+
+return 0;
+
+
+
 }
