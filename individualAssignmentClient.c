9323fbb90e3cd9031cf0cc53e63fbe4bafe8730e
bbxiuu <2021833992@student.uitm.edu.my>
2022-05-26 12:02:17 +0800

    Client

--- individualAssignmentClient.c
+++ individualAssignmentClient.c
@@ -1,20 +1,26 @@
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 int socQotd,new_socket,c;
 struct sockaddr_in server;
-char clmsg[20000],sv_reply[20000];
+string quotes[20000],sv_reply[20000];
 
-//create socket
+//creating socket
 socQotd=socket(AF_INET, SOCK_STREAM, 0);
 if(socQotd==-1)
 {
-printf("Could not create socket");
+printf("Failed to create socket!");
 }
 
 
@@ -30,31 +36,6 @@ return 1;
 }
 puts("Connected \n");
 
-
-//message to server
-while(1)
-{
-printf("Enter a message: ");
-fgets(clmsg,20000,stdin);
-send(socQotd,clmsg,20000,0);
-
-if(send(socQotd,clmsg,strlen(clmsg),0)<0)
-{
-puts("FAILED TO SEND");
-return 1;
-}
-
-if(recv(socQotd,sv_reply,20000,0)<0)
-{
-puts("recv failed");
-break;
-}
-
-puts("_________________________________________\n");
-puts("SERVER REPLY: ");
-puts(sv_reply);
-
-}
 
 close(socQotd);
 return 0;
