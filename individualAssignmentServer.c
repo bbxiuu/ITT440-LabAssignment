b9784612cf58d1ff38581af496caa3d73e315ec7
bbxiuu <bbxiuu@github.com>
2022-05-26 13:50:52 +0800

    server

--- individualAssignmentServer.c
+++ individualAssignmentServer.c
@@ -14,52 +14,53 @@
 
 int main(int argc, char *argv[] )
 {
-int socQotd,new_socket,c;
-struct sockaddr_in server, client;
+  int socQotd,new_socket,c;
+  struct sockaddr_in server, client;
 
-//creating socket
-socQotd = socket(AF_INET,SOCK_STREAM,0);
-if(socQotd==-1)
-{
+  //creating socket
+    socQotd = socket(AF_INET,SOCK_STREAM,0);
+    if(socQotd==-1)
+      {
 
-printf("Failed to create socket!");
+        printf("Failed to create socket!");
 
-}
+      }
 
-//preparing for socket address
-server.sin_family=AF_INET;
-server.sin_addr.s_addr=INADDR_ANY;
-server.sin_port=htons(17); //use port 17
+  //preparing for socket address
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=INADDR_ANY;
+    server.sin_port=htons(17); //use port 17
 
-//bind
-if(bind(socQotd,(struct sockaddr *)&server, sizeof(server))<0)
-{
+  //bind
+    if(bind(socQotd,(struct sockaddr *)&server, sizeof(server))<0)
+      {
 
-puts("bind failed");
-return 1;
-}
-puts("Socket is successfully binded! ");
-
-
-//listen
-listen (socQotd,3);
-
-//accept for incoming connection
-puts("Waiting for incoming connections....");
-c=sizeof(struct sockaddr_in);
+       puts("bind failed");
+       return 1;
+      }
+     puts("Socket is successfully binded! ");
 
 
+  //listen
+    listen (socQotd,3);
 
-//server client connection
-new_socket=accept(socQotd,(struct sockaddr *)&client,(socklen_t*)&c);
-if(new_socket<0)
-{
-perror("FAILED TO ACCEPT");
-return 1;
-}
-puts("Connection accepted");
+  //accept for incoming connection
+    puts("Waiting for incoming connections....");
+    c=sizeof(struct sockaddr_in);
 
-close(socQotd);
+
+  //server client connection
+    new_socket=accept(socQotd,(struct sockaddr *)&client,(socklen_t*)&c);
+    if(new_socket<0)
+      {
+       perror("FAILED TO ACCEPT");
+       return 1;
+      }
+    puts("Connection accepted");
+
+  //qotd protocol
+
+  close(socQotd);
 
 return 0;
 }
